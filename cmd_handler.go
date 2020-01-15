8217ed02547858c4cc6b64b103d5a1d169f9c6f1
hemendra619 <hemendra.chaudhary619@gmail.com>
2020-01-15 16:01:31 +0530

    command line working

--- cmd_handler.go
+++ cmd_handler.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strings"
+)
+
 func executeInlineCommands() error{
+	exitCommand := false
+	var buffReader *bufio.Reader
+	buffReader = bufio.NewReader(os.Stdin)
+
+	for !exitCommand {
+		inputText, _ := buffReader.ReadString('\n')
+		inputText = strings.TrimRight(inputText, "\r\n")
+		if inputText == "exit" {
+			break
+		}
+		command := splitCommand(inputText)
+		//fmt.Println(command)
+		output, err := runCommand(command)
+		if err != nil {
+			fmt.Println(err.Error())
+		}else{
+			fmt.Println(output)
+		}
+	}
 	return nil
 }
